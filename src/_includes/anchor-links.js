ad13d72c71c2fd66bf9d984f313092632ba77d0f
Ryan Collins <ryan@cloudcannon.com>
2019-06-10 22:51:18 +0000

    Updated 'src/_includes/anchor-links.js' via CloudCannon

--- src/_includes/anchor-links.js
+++ src/_includes/anchor-links.js
@@ -1,10 +1,14 @@
-var headings = document.querySelectorAll("h3[id], h4[id], h5[id], h6[id]");
-
-for (var i = 0; i < headings.length; i++) {
-	var anchorLink = document.createElement("a");
-	anchorLink.innerText = "#";
-	anchorLink.href = "#" + headings[i].id;
-	anchorLink.classList.add("header-link");
-
-	headings[i].appendChild(anchorLink);
+if (document.querySelector("body").classList.contains("cms-editor-active")) {
+    
+    var headings = document.querySelectorAll("h3[id], h4[id], h5[id], h6[id]");
+    
+    for (var i = 0; i < headings.length; i++) {
+    	var anchorLink = document.createElement("a");
+    	anchorLink.innerText = "#";
+    	anchorLink.href = "#" + headings[i].id;
+    	anchorLink.classList.add("header-link");
+    
+    	headings[i].appendChild(anchorLink);
+    }
 }
+
